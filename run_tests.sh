8cdc4e33eff3ce7c78c782c00286270811da8f5e
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-04-03 21:28:21 +0200

    task1: Test installation with travis

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-echo Nothing to test.
+# Error out when any command returns nonzero
+set -e
+
+echo Running tests for task 1...
+cd task1_installation
+sh test_installation.sh
+echo Done.
