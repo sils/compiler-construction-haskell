5b5c6e9e9b1704d4f69fc8e48054d66ff152ce3b
Timo Neuhäußer <timo.neuhaeusser@gmail.com>
2015-05-04 18:59:53 +0200

    test_task2: Execute tests for task2
    Add execution of test script for task2 to global test script

--- run_tests.sh
+++ run_tests.sh
@@ -6,4 +6,11 @@ set -e
 echo Running tests for task 1...
 cd task1_installation
 sh test_installation.sh
-echo Done.
+cd ..
+echo Done, tests for task 1.
+echo Running tests for task 2..
+cd task2_parser
+sh test_task2.sh
+cd ..
+echo Done, tests for task 2.
+
