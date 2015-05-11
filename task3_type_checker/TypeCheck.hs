80a09f13e40e0050364252c3b6962abf9c007352
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-05-11 15:11:17 +0200

    typecheck: add comments

--- task3_type_checker/TypeCheck.hs
+++ task3_type_checker/TypeCheck.hs
@@ -4,16 +4,20 @@ import AbsMini
 import PrintMini
 import ErrM
 
+-- The environment is a list of *scopes*. Each scope holds a list of
+-- Identifiers and their associated types.
 type Env = [ [ ( Ident , Type ) ] ]
 emptyEnv :: Env
 emptyEnv = [[]]
 
+-- Adds a variable to the uppermost scope of the given environment.
 addVar :: Env -> Ident -> Type -> Err Env
 addVar (scope:rest) identifier typ =
   case lookup identifier scope of
     Nothing -> return (((identifier, typ):scope):rest)
     Just _ -> fail ("Variable " ++ printTree identifier ++ " was already declared.")
 
+-- Looks up a variable in the given environment.
 lookupVar :: Env -> Ident -> Err TypeChecker
 lookupVar [] identifier = fail ("Unknown variable " ++ printTree identifier ++ ".")
 lookupVar (scope:rest) identifier =
@@ -21,8 +25,10 @@ lookupVar (scope:rest) identifier =
     Nothing -> lookupVar rest identifier
     Just typ -> return typ
 
+-- Adds a new empty scope to the environment.
 addScope :: Env -> Env
 addScope env = []:env
+
 
 typecheck :: Program -> Err ( )
 checkStms :: Env -> [ Stm ] -> Err ( )
