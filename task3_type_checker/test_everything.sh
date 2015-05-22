31b5bcb5b9a6cbc40fcfb2215173cdc6ba376908
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-05-22 12:18:36 +0200

    test_everything: Add bad examples

--- task3_type_checker/test_everything.sh
+++ task3_type_checker/test_everything.sh
@@ -3,8 +3,17 @@ set -e
 
 make
 
-FILES=lab2-testsuite/good/*
-for f in $FILES
+GOODFILES=lab2-testsuite/good/*
+for f in $GOODFILES
 do
     ./TestCPP $f
+done
+
+BADFILES=lab2-testsuite/bad/*
+for f in $BADFILES
+do
+    if ./TestCPP $f; then
+        echo "File $f was successfully type checked but it is a bad example!"
+        exit 1
+    fi
 done
