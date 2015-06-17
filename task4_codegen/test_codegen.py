b68542c905f07b8cff4aadc1097f4b7900d5ac00
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-06-18 01:40:22 +0200

    extend test script

--- task4_codegen/test_codegen.py
+++ task4_codegen/test_codegen.py
@@ -6,11 +6,17 @@ def call(command):
     return _call(command, shell=True)
 
 def main():
-    call("make")
-    call("./TestCPP ./testSuite/codeGenTest.c")
-    for i in range(8):
-        args = i*" anyarg"
-        assert call("lli TestOutput.ll"+args) == i+2
+    try:
+        assert call("make") == 0
+        assert call("./TestCPP ./testSuite/codeGenTest.c") == 0
+        for i in range(8):
+            args = i*" anyarg"
+            assert call("lli TestOutput.ll"+args) == i+2
+    except AssertionError:
+        print("Test failed!")
+        exit(1)
+
+    print("The code generator works according to this test suite.")
 
 if __name__ == "__main__":
     main()
